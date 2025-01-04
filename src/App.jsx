4aa52684842a27e3a643a1b73633406338a8d46d
Alwyn Dani Abraham <144090174+alwyndani@users.noreply.github.com>
2025-01-04 16:31:08 +0530

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return <>
-  <div><h1>Cryto verse berthvbinefvc</h1></div>
-  </>;
+  <div><h1>Cryto verse berthvbinefvc</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus autem aut sit laborum. Pariatur maiores repudiandae sed in iusto ad mollitia vel non perferendis a ullam voluptatum, officia minima quod!</p></div>
+  </>
 }
 
 export default App;
